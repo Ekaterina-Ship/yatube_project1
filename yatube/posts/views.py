f10609b2b52e4cc041459d60ea467bb7a7ed44fb
Екатерин Шипулина <eva_vs@mail.ru>
2022-10-11 20:29:33 +0300

    4 спринт близок финалу

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from .models import Post, Group, User
 
 
 def index(request):
@@ -23,3 +23,33 @@ def group_posts(request, slug):
         'page_obj': page_obj,
     }
     return render(request, 'posts/group_list.html', context)
+
+def profile(request, username):
+    author_id = User.objects.get(username=username)
+    posts = Post.objects.filter(author=author_id).all()
+    count = Post.objects.count()
+    paginator = Paginator(posts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    context = {"author": author_id,
+               "posts": posts,
+               "count": count,
+               "page_obj": page_obj,
+               }
+    return render(request, "posts/profile.html", context)
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    pub_date = post.pub_date
+    post_title = post.text[:30]
+    author = post.author
+    author_posts = author.posts.all().count()
+    context = {
+        "post": post,
+        "post_title": post_title,
+        "author": author,
+        "author_posts": author_posts,
+        "pub_date": pub_date,
+    }
+    return render(request, 'posts/post_detail.html', context)
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('about/', include('about.urls', namespace='about')),
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path('', include('posts.urls', namespace='posts')),
